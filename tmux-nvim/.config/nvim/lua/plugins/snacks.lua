d245a0eba1c6decd088c8750e401d094ab0df557
DevIsuruSampath <devisurusampath@gmail.com>
2026-02-11 13:24:15 +0000

    feat(nvim): add folke snacks with picker and notifier

---
+++ tmux-nvim/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    opts = {
+      bigfile = { enabled = true },
+      quickfile = { enabled = true },
+      statuscolumn = { enabled = true },
+      picker = { enabled = true },
+      notifier = { enabled = true },
+      input = { enabled = true },
+      dashboard = { enabled = false },
+      indent = { enabled = true },
+      words = { enabled = true },
+    },
+    keys = {
+      { "<leader><space>", function() Snacks.picker.files() end, desc = "Snacks: files" },
+      { "<leader>/", function() Snacks.picker.grep() end, desc = "Snacks: grep" },
+      { "<leader>n", function() Snacks.notifier.show_history() end, desc = "Snacks: notifications" },
+    },
+  },
+}
