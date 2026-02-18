197b77c55e274bfb9d7702bd5fe63e1bf8144073
DevIsuruSampath <DevIsuruSampath@gmail.com>
2026-02-19 01:23:25 +0530

    Enhance session handling options for persistence.nvim
    Added options for session handling in persistence.nvim plugin.

--- tmux-nvim/.config/nvim/lua/plugins/session.lua
+++ tmux-nvim/.config/nvim/lua/plugins/session.lua
@@ -1,13 +1,41 @@
 return {
   {
     "folke/persistence.nvim",
-    event = "BufReadPre",
-    opts = {},
+    event = "BufReadPre", -- Start recording when a file is opened
+    opts = {
+      -- Options for session handling
+      need = 1, -- Only save session if at least 1 file buffer is open
+      branch = true, -- Create separate sessions for different git branches
+    },
     keys = {
-      { "<leader>qs", function() require("persistence").load() end, desc = "Restore session" },
-      { "<leader>qS", function() require("persistence").select() end, desc = "Select session" },
-      { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore last session" },
-      { "<leader>qd", function() require("persistence").stop() end, desc = "Stop session save" },
+      {
+        "<leader>qs",
+        function()
+          require("persistence").load()
+        end,
+        desc = "Restore session",
+      },
+      {
+        "<leader>qS",
+        function()
+          require("persistence").select()
+        end,
+        desc = "Select session",
+      },
+      {
+        "<leader>ql",
+        function()
+          require("persistence").load({ last = true })
+        end,
+        desc = "Restore last session",
+      },
+      {
+        "<leader>qd",
+        function()
+          require("persistence").stop()
+        end,
+        desc = "Stop session save",
+      },
     },
   },
 }
