8e360ffe6881bdea2169c830acf51cd65fdb023c
DevIsuruSampath <DevIsuruSampath@gmail.com>
2026-02-19 01:00:49 +0530

    Update Noice plugin configuration for Neovim

--- tmux-nvim/.config/nvim/lua/plugins/noice.lua
+++ tmux-nvim/.config/nvim/lua/plugins/noice.lua
@@ -3,31 +3,45 @@ local is_termux = (vim.env.PREFIX or ""):match("com.termux") ~= nil
 return {
   {
     "folke/noice.nvim",
-    enabled = not is_termux,
     event = "VeryLazy",
+    enabled = not is_termux, -- DISABLE on Termux
     dependencies = {
       "MunifTanjim/nui.nvim",
       "rcarriga/nvim-notify",
     },
     opts = {
       lsp = {
-        progress = { enabled = true },
-        hover = { enabled = true },
-        signature = { enabled = true },
+        -- override markdown rendering so that cmp and other plugins use Treesitter
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
       },
+      -- Presets help configure Noice for common setups
       presets = {
-        bottom_search = true,
-        command_palette = true,
-        long_message_to_split = true,
+        bottom_search = true,         -- use a classic bottom cmdline for search
+        command_palette = true,       -- position the cmdline and popupmenu together
+        long_message_to_split = true, -- long messages will be sent to a split
+        inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = false,       -- add a border to hover docs and signature help
       },
     },
   },
   {
     "rcarriga/nvim-notify",
-    enabled = not is_termux,
+    enabled = not is_termux, -- DISABLE on Termux
     opts = {
-      timeout = 2500,
-      background_colour = "#000000",
+      timeout = 3000,
+      max_height = function()
+        return math.floor(vim.o.lines * 0.75)
+      end,
+      max_width = function()
+        return math.floor(vim.o.columns * 0.75)
+      end,
+      on_open = function(win)
+        vim.api.nvim_win_set_config(win, { zindex = 100 })
+      end,
     },
   },
 }
